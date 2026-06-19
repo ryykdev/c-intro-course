d4a8bf9868621af115b80158ceef791db9fa5f49
Raphael <raphaelreichdev@gmail.com>
2026-06-19 11:43:53 +0200

    sync

--- queen-attack/queen_attack.c
+++ queen-attack/queen_attack.c
@@ -6,9 +6,8 @@ attack_status_t can_attack(position_t queen_1, position_t queen_2) {
   if (queen_1.row == queen_2.row && queen_1.column == queen_2.column)
     return INVALID_POSITION;
   // restricted to board size
-  if (queen_1.row < 0 || queen_1.row > 7 || queen_1.column < 0 ||
-      queen_1.column > 7 || queen_2.row < 0 || queen_2.row > 7 ||
-      queen_2.column < 0 || queen_2.column > 7)
+  if (queen_1.row > 7 || queen_1.column > 7 || queen_2.row > 7 ||
+      queen_2.column > 7)
     return INVALID_POSITION;
 
   // queens can attack lines
